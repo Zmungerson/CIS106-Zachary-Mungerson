bf6ae2ac9358e0163b1236ba17eeef0050e3f574
Zmungerson <zmungerson@gmail.com>
2024-05-25 20:53:30 +0200

    Add files via upload

---
+++ Week 1/PS1P4.py
@@ -0,0 +1,8 @@
+#Input
+Lname = input("Enter Last Name: ")
+Credithours = float(input("Enter Credit Hours: "))
+#Process
+Tuition = Credithours * 250 +100
+#Output
+print(Lname)
+print("The Total Tution is $", Tuition)
