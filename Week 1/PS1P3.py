c4b014d4056b8c4665bc0ef75e09dd858b469c2d
Zmungerson <zmungerson@gmail.com>
2024-05-25 20:49:34 +0200

    Add files via upload

---
+++ Week 1/PS1P3.py
@@ -0,0 +1,9 @@
+#Input
+Length = float(input("Enter Length of Rectangle: "))
+Width = float(input("Enter Width of Rectangle: "))
+#Process
+Area = Length * Width
+Circumference = 2 * (Length + Width)
+#Output
+print("The Area of the Rectangle is", Area)
+print("The Circumference of the Rectangle is", Circumference)
