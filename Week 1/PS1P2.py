066ba2edf26d84eafb2c65cfac0f3d1693b4c44a
Zmungerson <zmungerson@gmail.com>
2024-05-25 20:45:46 +0200

    Add files via upload

---
+++ Week 1/PS1P2.py
@@ -0,0 +1,9 @@
+#Input
+Lname = input("Enter Last Name: ")
+Hours = float(input("Enter Hours Worked: "))
+Rate = float(input("Enter Hourly Rate: "))
+#Process
+Gross = Hours * Rate
+#Output
+print(Lname)
+print("Gross Pay: $", Gross)
