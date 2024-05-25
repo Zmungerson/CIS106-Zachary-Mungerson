1606a24bb94d8da3ee6833504845b82be9786eff
Zmungerson <zmungerson@gmail.com>
2024-05-25 20:36:56 +0200

    Add files via upload

---
+++ Week 1/PS1P1.py
@@ -0,0 +1,7 @@
+#Input
+Price = float(input("Enter Price of Item: "))
+Quantity = float(input("Enter Quantity of Item: "))
+#Process
+Extprice = Price * Quantity
+#Output
+print("The Extended Price is $", Extprice)
